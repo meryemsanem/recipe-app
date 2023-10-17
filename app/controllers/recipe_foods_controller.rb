fb89e20e7982a88b7c84baa73fed7cf1a045e119
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-17 13:53:24 +0300

    Create show view for recipes and create RecipeFoodController

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,11 +1,45 @@
 class RecipeFoodsController < ApplicationController
-  def index; end
+  before_action :set_recipe_food, only: [:destroy, :edit, :update]
 
-  def show; end
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @foods = Food.all
+    @recipe_food = @recipe.recipe_foods.new
+  end
 
-  def new; end
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
 
-  def create; end
+    if @recipe.user == current_user && @recipe_food.save
+      redirect_to @recipe, notice: 'Ingredient successfully added'
+    else
+      @foods = Food.all
+    end
+  end
 
-  def destroy; end
+
+  def update
+    if @recipe.user == current_user && @recipe_food.update(recipe_food_params)
+      redirect_to @recipe, notice: 'Ingredient updated successfully'
+    else
+      @foods = Food.all
+    end
+  end
+
+  def destroy
+    @recipe_food.destroy
+    redirect_to @recipe, notice: 'Ingredient deleted successfully'
+  end
+
+  private
+
+  def set_recipe_food
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe = @recipe_food.recipe
+  end
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :recipe_id, :food_id)
+  end
 end
