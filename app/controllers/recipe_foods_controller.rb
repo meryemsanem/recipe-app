2beec07d0ecd5b94d687009b5bf0861d8228e5a6
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-17 15:17:42 +0300

    Update RecipeFoodsController for modify option

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,45 +1,51 @@
 class RecipeFoodsController < ApplicationController
-  before_action :set_recipe_food, only: [:destroy, :edit, :update]
+  before_action :find_recipe
+  before_action :find_recipe_food, only: [:edit, :update, :destroy]
 
   def new
-    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.new
     @foods = Food.all
-    @recipe_food = @recipe.recipe_foods.new
   end
 
   def create
-    @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
-
-    if @recipe.user == current_user && @recipe_food.save
-      redirect_to @recipe, notice: 'Ingredient successfully added'
+    if @recipe_food.save
+      redirect_to recipe_path(@recipe), notice: 'Ingredient successfully added.'
     else
       @foods = Food.all
+      render :new
     end
   end
 
+  def edit
+    @foods = Food.all
+  end
 
   def update
-    if @recipe.user == current_user && @recipe_food.update(recipe_food_params)
-      redirect_to @recipe, notice: 'Ingredient updated successfully'
+    if @recipe_food.update(recipe_food_params)
+      redirect_to recipe_path(@recipe), notice: 'Ingredient updated successfully.'
     else
       @foods = Food.all
+      render :edit
     end
   end
 
   def destroy
     @recipe_food.destroy
-    redirect_to @recipe, notice: 'Ingredient deleted successfully'
+    redirect_to recipe_path(@recipe), notice: 'Ingredient deleted successfully.'
   end
 
   private
 
-  def set_recipe_food
-    @recipe_food = RecipeFood.find(params[:id])
-    @recipe = @recipe_food.recipe
+  def find_recipe
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def find_recipe_food
+    @recipe_food = @recipe.recipe_foods.find(params[:id])
   end
 
   def recipe_food_params
-    params.require(:recipe_food).permit(:quantity, :recipe_id, :food_id)
+    params.require(:recipe_food).permit(:food_id, :quantity)
   end
 end
