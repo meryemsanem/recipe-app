67e6bb2273906d3e8ca76dc8ad28b5961c65d17b
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-17 12:19:30 +0300

    Add validations to Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,10 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
+
   belongs_to :user
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
 end
