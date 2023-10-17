a8f6620157a4d40e1f8084c52a04be10180c7386
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-17 16:27:19 +0300

    Update manifest.js to run the server

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,10 @@
-document.addEventListener('turbo:load', function() {
+//= link application.css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+
+document.addEventListener('turbo:load', function () {
   const toggleSwitch = document.querySelector('.toggle-switch');
   const toggleForm = document.querySelector('.toggle-form');
   if (toggleSwitch && toggleForm) {
