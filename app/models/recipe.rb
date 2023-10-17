1d3123221a2f7443b9426fe2406abbc24f08dc05
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-17 12:36:08 +0300

    Generate migration to change data type of cooking time and preparation time to decimal

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
-  validates :preparation_time, presence: true
-  validates :cooking_time, presence: true
+  validates :preparation_time, presence: true, numericality: true
+  validates :cooking_time, presence: true, numericality: true
   validates :description, presence: true
 
   belongs_to :user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_16_130630) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_17_093043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_130630) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.integer "preparation_time"
-    t.integer "cooking_time"
+    t.decimal "preparation_time"
+    t.decimal "cooking_time"
     t.text "description"
     t.boolean "public"
     t.bigint "user_id", null: false
