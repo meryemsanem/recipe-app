79995ed54e550ef29c9c428a7b5c4134420aeed3
Diana Beki <dianabeki54@gmail.com>
2023-10-18 12:01:04 +0300

    Fix linters

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,7 +6,6 @@ class Food < ApplicationRecord
   validates :price, presence: true
   validates :measurement_unit, presence: true
   validates :quantity, presence: true
-  
 
   def self.total_price
     sum(:price)
