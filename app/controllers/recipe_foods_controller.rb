34f8cac2b0388dfbc26cc1c124442bf00681de89
Diana Beki <dianabeki54@gmail.com>
2023-10-18 13:55:49 +0300

    Secure app from n+1 problem

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,12 +1,10 @@
 class RecipeFoodsController < ApplicationController
   before_action :find_recipe
   before_action :find_recipe_food, only: %i[edit update destroy]
-
   def new
     @recipe_food = RecipeFood.new
-    @foods = Food.all
+    @foods = Food.all.includes(:recipe_foods)
   end
-
   def create
     @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
     if @recipe_food.save
@@ -16,11 +14,9 @@ class RecipeFoodsController < ApplicationController
       render :new
     end
   end
-
   def edit
     @foods = Food.all
   end
-
   def update
     if @recipe_food.update(recipe_food_params)
       redirect_to recipe_path(@recipe), notice: 'Ingredient updated successfully.'
@@ -29,22 +25,17 @@ class RecipeFoodsController < ApplicationController
       render :edit
     end
   end
-
   def destroy
     @recipe_food.destroy
     redirect_to recipe_path(@recipe), notice: 'Ingredient deleted successfully.'
   end
-
   private
-
   def find_recipe
     @recipe = Recipe.find(params[:recipe_id])
   end
-
   def find_recipe_food
     @recipe_food = @recipe.recipe_foods.find(params[:id])
   end
-
   def recipe_food_params
     params.require(:recipe_food).permit(:food_id, :quantity)
   end
