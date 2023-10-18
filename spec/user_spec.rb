6f757d787ca390a1d6ba32158953af26b94bf07c
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-18 18:06:37 +0300

    Add unit tests for User model associations

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -12,4 +12,22 @@ RSpec.describe User, type: :model do
       expect(user).not_to be_valid
     end
   end
+
+  describe 'Associations' do
+    it 'has many foods' do
+      association = described_class.reflect_on_association(:foods)
+      expect(association.macro).to eq(:has_many)
+    end
+
+    it 'has many recipes' do
+      association = described_class.reflect_on_association(:recipes)
+      expect(association.macro).to eq(:has_many)
+    end
+
+    it 'has many recipe_foods through recipes' do
+      association = described_class.reflect_on_association(:recipe_foods)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:through]).to eq(:recipes)
+    end
+  end
 end
