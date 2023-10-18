abf7860a057db4157fa2160c84e02fc3d4e02bb5
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-18 18:12:33 +0300

    Add unit tests for Recipe model associations

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -49,6 +49,23 @@ RSpec.describe Recipe, type: :model do
       expect(recipe).not_to be_valid
     end
   end
+    describe 'Associations' do
+    it 'belongs to a user' do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'has many recipe_foods' do
+      association = described_class.reflect_on_association(:recipe_foods)
+      expect(association.macro).to eq(:has_many)
+    end
+
+    it 'has many foods through recipe_foods' do
+      association = described_class.reflect_on_association(:foods)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:through]).to eq(:recipe_foods)
+    end
+  end
 end
 
 
